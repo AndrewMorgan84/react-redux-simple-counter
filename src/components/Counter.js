49bc78620b5c5bde875faca5f8e82f7a30c6d6ba
Andrew <andrew.morgan@e.org>
2022-02-14 20:51:30 +0000

    implemented multiple type state redux functions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -4,25 +4,34 @@ import classes from "./Counter.module.css";
 const Counter = () => {
   const dispatch = useDispatch();
   const counter = useSelector((state) => state.counter);
-  const toggleCounterHandler = () => {};
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
     dispatch({ type: "INCREMENT" });
+  };
+
+  const increaseHandler = () => {
+    dispatch({ type: "INCREASE", amount: 10 });
   };
 
   const decrementHandler = () => {
     dispatch({ type: "DECREMENT" });
   };
 
+  const toggleHandler = () => {
+    dispatch({ type: "TOGGLE" });
+  };
+
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment +</button>
+        <button onClick={increaseHandler}>Increase By Ten</button>
         <button onClick={decrementHandler}>Decrement -</button>
       </div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
+      <button onClick={toggleHandler}>Toggle Counter</button>
     </main>
   );
 };
